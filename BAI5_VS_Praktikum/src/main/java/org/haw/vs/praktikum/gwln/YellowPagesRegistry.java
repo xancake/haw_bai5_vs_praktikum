e78e067f5921e0ce3ed732c13c9c9e89a3a5f447
Xancake <xanylein@web.de>
2016-04-06 23:16:20 +0200

    Yellow-Pages Registrierung extrahiert und Web-Service-Methoden ausgelagert
    - Der Yellow-Pages dürfte aber noch nicht funktionieren

--- BAI5_VS_Praktikum/src/main/java/org/haw/vs/praktikum/gwln/YellowPagesRegistry.java
+++ BAI5_VS_Praktikum/src/main/java/org/haw/vs/praktikum/gwln/YellowPagesRegistry.java
@@ -1,13 +1,82 @@
 package org.haw.vs.praktikum.gwln;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+
 public class YellowPagesRegistry {
 	private static final String YELLOW_PAGES = "http://172.18.0.5:4567/services";
 	
-	public boolean isServiceRegistered() {
-		
+	public static String registerOrUpdateService(String name, String description, String service, String uri) {
+		if(isServiceRegistered(name, description, service, uri)) {
+			String id = getServiceId(name, description, service, uri);
+			updateServiceRegistry(id, name, description, service, uri);
+			return id;
+		} else {
+			registerService(name, description, service, uri);
+			return getServiceId(name, description, service, uri);
+		}
 	}
 	
-	public void registerService() {
+	public static boolean isServiceRegistered(String name, String description, String service, String uri) {
+		try {
+			HttpResponse<JsonNode> jsonResponse = Unirest.get(YELLOW_PAGES + "/of/name/" + name).asJson();
+			if(jsonResponse.getStatus() == 200) {
+				JSONArray services = jsonResponse.getBody().getArray();
+				if(services.length() > 0) {
+					// TODO: auf einem toten Service registrieren (put)
+					// Wenn es keinen gibt, müssen wir evtl. trotzdem putten
+				}
+				return false;
+			}
+			throw new IllegalArgumentException("Yellow-Pages lieferte Statuscode '" + jsonResponse.getStatus() + "'");
+		} catch(UnirestException e) {
+			// TODO: besseren Exceptiontypen, idealerweise eine checked-Exception
+			throw new RuntimeException("Yellow-Pages Registry nicht erreichbar", e);
+		}
+	}
+	
+	public static String getServiceId(String name, String description, String service, String uri) {
 		
+		
+		
+		return null;
+	}
+	
+	public static void registerService(String name, String description, String service, String uri) {
+		try {
+			HttpResponse<String> response = Unirest.post(YELLOW_PAGES)
+					.header("Content-Type", "application/json")
+					.body(createYellowPagesJSON(name, description, service, uri))
+					.asString();
+		} catch (UnirestException e) {
+			// TODO: besseren Exceptiontypen, idealerweise eine checked-Exception
+			throw new RuntimeException("Yellow-Pages Registry nicht erreichbar", e);
+		}
+	}
+	
+	public static void updateServiceRegistry(String id, String name, String description, String service, String uri) {
+		try {
+			HttpResponse<String> response = Unirest.put(YELLOW_PAGES + "/" + id)
+					.header("Content-Type", "application/json")
+					.body(createYellowPagesJSON(name, description, service, uri))
+					.asString();
+		} catch (UnirestException e) {
+			// TODO: besseren Exceptiontypen, idealerweise eine checked-Exception
+			throw new RuntimeException("Yellow-Pages Registry nicht erreichbar", e);
+		}
+	}
+	
+	private static JSONObject createYellowPagesJSON(String name, String description, String service, String uri) {
+		JSONObject json = new JSONObject();
+		json.put("name", name);
+		json.put("description", description);
+		json.put("service", service);
+		json.put("uri", uri);
+		return json;
 	}
 }
--- BAI5_VS_Praktikum/src/main/java/org/haw/vs/praktikum/gwln/praktikum1/b/EventManagerWebService.java
+++ BAI5_VS_Praktikum/src/main/java/org/haw/vs/praktikum/gwln/praktikum1/b/EventManagerWebService.java
@@ -1,101 +1,79 @@
 package org.haw.vs.praktikum.gwln.praktikum1.b;
 
-import static spark.Spark.delete;
-import static spark.Spark.get;
-import static spark.Spark.post;
+import static spark.Spark.*;
 
 import java.util.List;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
+import org.haw.vs.praktikum.gwln.YellowPagesRegistry;
+
+import spark.Request;
+import spark.Response;
 
 import com.google.gson.Gson;
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class EventManagerWebService {
-	private static final String YELLOW_PAGES = "http://172.18.0.5:4567/services";
-	
 	private static final String NAME = "Event Manager Service 42_1337_69";
 	private static final String DESCRIPTION = "Bester Event Manager 42_1337_69";
 	private static final String SERVICE = "Event Manager Service";
 	private static final String URI = "http://abq335_events:4567/events";
 	
+	private static final EventManager MANAGER = new EventManager();
+	
+	private static String postEvent(Request request, Response response) {
+		String game = request.queryParams("game");
+		String type = request.queryParams("type");
+		String name = request.queryParams("name");
+		String reason  = request.queryParams("request");
+		String resource = request.queryParams("resource");
+		String player = request.queryParams("player");
+		String time = request.queryParams("time");
+		
+		Event event = new Event(String.valueOf(MANAGER.getSize()+1), game, type, name, reason, resource, player, time);
+		MANAGER.addEvent(event);
+		
+		return "ok";
+	}
+	
+	private static String getEvents(Request request, Response response) {
+		String game = request.queryParams("game");
+		String type = request.queryParams("type");
+		String name = request.queryParams("name");
+		String reason  = request.queryParams("request");
+		String resource = request.queryParams("resource");
+		String player = request.queryParams("player");
+		
+		List<Event> filteredEvents = MANAGER.getMatchingEvents(game, type, name, reason, resource, player);
+		
+		response.status(200);
+		return new Gson().toJson(filteredEvents);
+	}
+	
+	private static String deleteEvent(Request request, Response response) {
+		String game = request.queryParams("game");
+		String type = request.queryParams("type");
+		String name = request.queryParams("name");
+		String reason  = request.queryParams("request");
+		String resource = request.queryParams("resource");
+		String player = request.queryParams("player");
+		
+		MANAGER.deleteEvent(game, type, name, reason, resource, player);
+		
+		return "ok";
+	}
+	
+	private static String getEvent(Request request, Response response) {
+		String id = request.params(":eventid");
+		
+		Event event = MANAGER.getEvent(id);
+		
+		return new Gson().toJson(event);
+	}
+	
 	public static void main(String[] args) {
-		try {
-			HttpResponse<JsonNode> jsonResponse = Unirest.get(YELLOW_PAGES+"/of/name/"+NAME).asJson();
-			JSONObject json = new JSONObject();
-			json.put("name", NAME);
-			json.put("description", DESCRIPTION);
-			json.put("service", SERVICE);
-			json.put("uri", URI);
-			if(jsonResponse.getStatus() == 200){
-				JSONArray services = jsonResponse.getBody().getArray();
-				if(services.length()>0) {
-					// TODO: auf einem toten Service registrieren (put)
-					// Wenn es keinen gibt, müssen wir evtl. trotzdem putten
-				} else {
-					HttpResponse<String> response = Unirest.post(YELLOW_PAGES)
-							.header("Content-Type", "application/json")
-							.body(json)
-							.asString();
-				}
-			}
-		} catch (UnirestException e) {
-			e.printStackTrace();
-		}
-		
-		EventManager manager = new EventManager();
-		Gson gson = new Gson();
-		
-		post("/events", (request,response) -> {
-			String game = request.queryParams("game");
-			String type = request.queryParams("type");
-			String name = request.queryParams("name");
-			String reason  = request.queryParams("request");
-			String resource = request.queryParams("resource");
-			String player = request.queryParams("player");
-			String time = request.queryParams("time");
-			
-			Event event = new Event(String.valueOf(manager.getSize()+1), game, type, name, reason, resource, player, time);
-			
-			manager.addEvent(event);
-			
-			return "ok";
-		});
-		get("/events", (request, response) -> {
-			String game = request.queryParams("game");
-			String type = request.queryParams("type");
-			String name = request.queryParams("name");
-			String reason  = request.queryParams("request");
-			String resource = request.queryParams("resource");
-			String player = request.queryParams("player");
-			
-			List<Event> filteredEvents = manager.getMatchingEvents(game, type, name, reason, resource, player);
-			
-			response.status(200);
-			return gson.toJson(filteredEvents);
-		});
-		delete("/events", (request, response) -> {
-			String game = request.queryParams("game");
-			String type = request.queryParams("type");
-			String name = request.queryParams("name");
-			String reason  = request.queryParams("request");
-			String resource = request.queryParams("resource");
-			String player = request.queryParams("player");
-			
-			manager.deleteEvent(game, type, name, reason, resource, player);
-			
-			return "ok";
-		});
-		get("/events/:eventid", (request, response) -> {
-			String id = request.params(":eventid");
-			
-			Event event = manager.getEvent(id);
-			
-			return gson.toJson(event);
-		});
+		YellowPagesRegistry.registerOrUpdateService(NAME, DESCRIPTION, SERVICE, URI);
+		post("/events", EventManagerWebService::postEvent);
+		get("/events", EventManagerWebService::getEvents);
+		delete("/events", EventManagerWebService::deleteEvent);
+		get("/events/:eventid", EventManagerWebService::getEvent);
 	}
 }
