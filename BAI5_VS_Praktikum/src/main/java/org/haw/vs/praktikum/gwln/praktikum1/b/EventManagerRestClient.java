42b68382f91cdde328a25070b658f5fe468f7239
gery <paradoxgery@gmx.net>
2016-04-27 16:39:14 +0200

    Die URL des EventManagerRestClient enthält nun bereits das "/events"

--- BAI5_VS_Praktikum/src/main/java/org/haw/vs/praktikum/gwln/praktikum1/b/EventManagerRestClient.java
+++ BAI5_VS_Praktikum/src/main/java/org/haw/vs/praktikum/gwln/praktikum1/b/EventManagerRestClient.java
@@ -8,34 +8,38 @@ import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 
 public class EventManagerRestClient {
-	private String _url;
+	private String _url; // Die Adresse muss "/events" beinhalten
 	
+	/**
+	 * 
+	 * @param url Die URL des "/events"-Services
+	 */
 	public EventManagerRestClient(String url) {
-		_url = url;
+		_url = url.endsWith("/events") ? url : url + "/events";
 	}
 	
 	public void postEvent(Event event) throws UnirestException {
-		Unirest.post(_url + "/events")
+		Unirest.post(_url)
 				.header("Content-Type", "application/json")
 				.body(EventJsonMarshaller.marshall(event))
 				.asString();
 	}
 	
 	public Event getEvent(String id) throws UnirestException {
-		HttpResponse<JsonNode> response = Unirest.get(_url + "/events/" + id).asJson();
+		HttpResponse<JsonNode> response = Unirest.get(_url + "/" + id).asJson();
 		return EventJsonMarshaller.unmarshall(response.getBody().getObject());
 	}
 	
 	@SuppressWarnings("unchecked")
 	public List<Event> getEvents(String game, String type, String name, String reason, String resource, String player) throws UnirestException {
 		String parameters = createParameterString(game, type, name, reason, resource, player);
-		HttpResponse<String> response = Unirest.get(_url + "/events" + parameters).asString();
+		HttpResponse<String> response = Unirest.get(_url + parameters).asString();
 		return (List<Event>)new Gson().fromJson(response.getBody(), List.class);
 	}
 	
 	public void deleteEvents(String game, String type, String name, String reason, String resource, String player) throws UnirestException {
 		String parameters = createParameterString(game, type, name, reason, resource, player);
-		Unirest.delete(_url + "/events" + parameters).asString();
+		Unirest.delete(_url + parameters).asString();
 	}
 	
 	private String createParameterString(String game, String type, String name, String reason, String resource, String player) {
