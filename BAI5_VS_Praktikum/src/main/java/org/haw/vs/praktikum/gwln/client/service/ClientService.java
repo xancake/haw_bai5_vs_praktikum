32f5c103c272210476fd7c6f0275f0bf04394d43
gery <paradoxgery@gmx.net>
2016-05-13 12:25:25 +0200

    client as a service
    routes for being able to listen to service calls
    stop for clearing routes
    listener for turn and event

---
+++ BAI5_VS_Praktikum/src/main/java/org/haw/vs/praktikum/gwln/client/service/ClientService.java
@@ -0,0 +1,50 @@
+package org.haw.vs.praktikum.gwln.client.service;
+
+import static spark.Spark.post;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.jetty.http.HttpStatus;
+import org.haw.vs.praktikum.gwln.events.Event;
+import org.haw.vs.praktikum.gwln.events.rest.EventJsonMarshaller;
+
+import spark.Request;
+import spark.Response;
+import spark.Spark;
+
+public class ClientService {
+
+	private List<ClientServiceListener_I> listeners = new ArrayList<>();
+	private EventJsonMarshaller eventJsonMarshaller = new EventJsonMarshaller();
+
+	private String clientTurn(Request request, Response response) {
+		listeners.forEach( (listener) -> listener.onTurn() );
+		response.status(HttpStatus.OK_200);
+		return "OK";
+	}
+
+	private String clientEvent(Request request, Response response) {
+		Event event = eventJsonMarshaller.unmarshall(request.body());
+		listeners.forEach( (listener) -> listener.onEvent(event) );
+		response.status(HttpStatus.OK_200);
+		return "OK";
+	}
+
+	public void start() {
+		post("/client/turn", 	(request, response) -> clientTurn(request, response) );
+		post("/client/event", 	(request, response) -> clientEvent(request, response) );
+	}
+	
+	public void stop() {
+		Spark.stop();
+	}
+
+	public void addListener(ClientServiceListener_I l) {
+		listeners.add(l);
+	}
+
+	public void removeListener(ClientServiceListener_I l) {
+		listeners.remove(l);
+	}
+}
